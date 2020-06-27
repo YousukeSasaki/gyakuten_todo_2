de6c189aaa3fd57778f688d5fedd636743830090
YosukeSasaki <yosaprog@gmail.com>
2020-06-27 16:01:32 +0900

    003
    rails g model task
    rails db:migrate

---
+++ db/migrate/20200620164233_create_tasks.rb
@@ -0,0 +1,11 @@
+class CreateTasks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tasks do |t|
+      t.string :title, null: false, limit: 30
+      t.string :description, limit: 100
+      t.datetime :deadline, null: false
+      t.integer :category_id
+      t.timestamps
+    end
+  end
+end
