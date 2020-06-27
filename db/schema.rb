78c1ce87ad7b01316feb0fe54aeb57980afa7be3
YosukeSasaki <yosaprog@gmail.com>
2020-06-27 16:06:31 +0900

    004
    rails g model category
    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_20_164233) do
+ActiveRecord::Schema.define(version: 2020_06_27_070421) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "color_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title", limit: 30, null: false
