97b5ed708cd6b5864b4b09ec122b62dc8b81c62a
YosukeSasaki <yosaprog@gmail.com>
2020-06-29 00:48:02 +0900

    007

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'tasks#index'
+  resources 'tasks', except: :show
+  resources 'categories', only: [:index, :edit, :update]
 end
